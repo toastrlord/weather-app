fbaef84487819860908c59d05f38ad9a72b3b9df
Rob Morandini <robjmorandini@hotmail.com>
2021-04-02 12:35:07 -0400

    Fix up configuration so babel works properly

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'main.js',
+        publicPath: '/'
+    },
+    devServer: {
+      contentBase: './dist',  
     },
     module: {
         rules: [
