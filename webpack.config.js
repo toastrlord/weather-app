ff28e2738bb6de51c3b339fe5bd40bdef2bc7e0a
Rob Morandini <robjmorandini@hotmail.com>
2021-04-02 12:18:45 -0400

    Add dependencies for reacact, adjust start scripts to use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,34 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: {
+                    
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ]
 }
